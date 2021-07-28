e145405e044eb6d006b199ac7946a4f31e89a2be
Mayank <68742521+minku1219@users.noreply.github.com>
2021-07-28 14:29:50 +0530

    Add files via upload

---
+++ iterate.py
@@ -0,0 +1,15 @@
+import subprocess
+while(True):
+    print("Press ctrl+c to Exit from Code")
+    n=input("Enter the Command: $ ")
+    s=subprocess.getstatusoutput(n)
+    if (n.isdigit()==True):
+        print(int(n)*2)
+    elif(s[0]==0):
+        op=subprocess.getstatusoutput(n)
+        if(op[0]==0):
+            print(op[1]);
+    else:
+        re=n.replace(" ",'+')
+        print(subprocess.getoutput('google-chrome https://www.google.com/search?q='+re))
+
